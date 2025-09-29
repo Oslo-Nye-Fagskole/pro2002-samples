d427397cadfe1aba3e874dfb7eb2af8d28115c24
Reza Mirzaei <rezmir96@oslonf.no>
2025-09-29 11:40:34 +0200

    Examples

---
+++ Module 3/Pylint Duplicate Test/invoice.py
@@ -0,0 +1,10 @@
+# invoice.py
+def compute_total(items, tax=0.25, discount=0.1):
+    total = 0
+    for it in items:
+        subtotal = it.price_cents * it.qty
+    if subtotal > 1000:
+        subtotal = int(subtotal * (1 - discount))
+    total += subtotal
+    total = int(total * (1 + tax))
+    return total
