31ab82c57e89f89adbfe79bd8be85409bfa913e7
Reza Mirzaei <rezmir96@oslonf.no>
2025-10-08 12:31:36 +0200

    README, comments

---
+++ Module 4/shop_api_mock/README.md
@@ -0,0 +1,8 @@
+# Shop API Mock
+
+## Run instructions
+
+```bash
+pip install -r requirements.txt
+flask run
+```
--- Module 4/shop_api_mock/shop/routes.py
+++ Module 4/shop_api_mock/shop/routes.py
@@ -2,10 +2,20 @@
 from flask import Blueprint, jsonify
 from shop.services import get_all_products
 
-# Blueprint groups routes for this part of the app
+# A Blueprint is a modular way to group related routes
+# It lets us separate related features like products, users and orders into different files
+# Each blueprint can later be registered in the main app, further improving project structure
+# FOr this mock example we create a generic one named "api" that will hold all our API routes
 api_bp = Blueprint("api", __name__)
 
 # Return all available products as JSON
+# The decorator @api_bp.get("/products") tells Flask that:
+# - when an HTTP GET request comes to "/products"
+# - use this function to handle it
+# We registers this blueprint in our app.py, so:
+# - Flask merges it into the main URL map
+# - When a client goes to http://localhost:5000/products (if running locally)
+# - Flask finds this route and calls list_products()
 @api_bp.get("/products")
 def list_products():
     products = get_all_products()
